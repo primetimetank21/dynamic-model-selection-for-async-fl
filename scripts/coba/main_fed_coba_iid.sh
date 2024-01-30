3af1f4aba5348c6c5b916d470041ea09f94cb35e
TanK <theflashtank21@gmail.com>
2024-01-30 01:48:26 -0500

    chore(refactor): modified COBA iid script for experimental runs

--- scripts/coba/main_fed_coba_iid.sh
+++ scripts/coba/main_fed_coba_iid.sh
@@ -2,6 +2,8 @@
 
 NUM_USERS=100
 SHARDS=2
+SEED=$1 # already done: 0,1,2,3,4,5,6,7,8,9,10
+EPOCHS=1000
 while getopts s:n: option; do
   case "${option}" in
     s) SHARDS=${OPTARG};;
@@ -9,11 +11,14 @@ while getopts s:n: option; do
   esac
 done
 
+# for SEED in 0 1 2 3 4 5 6 7 8 9 10; do
 # "Best" case
-python3 main_fed.py --dataset coba --model cnn --num_classes 14 --log_level info --epochs 1000 --lr 0.1 --num_users 100 --shard_per_user 2 --frac 0.3 --local_ep 1 --local_bs 10 --results_save coba_fedavg_bestcase_run1 --iid
+python3 main_fed.py --seed $SEED --dataset coba --model cnn --num_classes 14 --log_level info --epochs $EPOCHS --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 0.3 --local_ep 1 --local_bs 10 --results_save coba_fedavg_bestcase_run1 --iid
 
 # "Average" case
-python3 main_fed.py --dataset coba --model cnn --num_classes 14 --log_level info --epochs 1000 --lr 0.1 --num_users 100 --shard_per_user 2 --frac 0.5 --local_ep 1 --local_bs 10 --results_save coba_fedavg_averagecase_run1 --iid
+python3 main_fed.py --seed $SEED --dataset coba --model cnn --num_classes 14 --log_level info --epochs $EPOCHS --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 0.5 --local_ep 1 --local_bs 10 --results_save coba_fedavg_averagecase_run1 --iid
 
 # "Worst" case
-python3 main_fed.py --dataset coba --model cnn --num_classes 14 --log_level info --epochs 1000 --lr 0.1 --num_users 100 --shard_per_user 2 --frac 1 --local_ep 1 --local_bs 10 --results_save coba_fedavg_worstcase_run1 --iid
+python3 main_fed.py --seed $SEED --dataset coba --model cnn --num_classes 14 --log_level info --epochs $EPOCHS --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 1.0 --local_ep 1 --local_bs 10 --results_save coba_fedavg_worstcase_run1 --iid
+
+# done
