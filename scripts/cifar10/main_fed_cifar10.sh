88690a9893ee626006441f411a09932c1420d41b
TanK <theflashtank21@gmail.com>
2024-03-19 21:28:01 -0400

    feat: added/modified multiple notebooks for results analysis

--- scripts/cifar10/main_fed_cifar10.sh
+++ scripts/cifar10/main_fed_cifar10.sh
@@ -12,12 +12,12 @@ done
 
 # for SEED in 0 1 2 3 4 5 6 7 8 9 10; do
 # "Best" case
-python3 main_fed.py --seed $SEED --dataset cifar10 --model cnn --num_classes 10 --log_level info --epochs 2000 --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 0.3 --local_ep 1 --local_bs 50 --results_save cifar10_fedavg_bestcase_run1
+python3 main_fed.py --seed $SEED --dataset cifar10 --model cnn --num_classes 10 --log_level info --epochs 1000 --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 0.3 --local_ep 1 --local_bs 50 --results_save cifar10_fedavg_bestcase_run1
 
 # "Average" case
-python3 main_fed.py --seed $SEED --dataset cifar10 --model cnn --num_classes 10 --log_level info --epochs 2000 --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 0.5 --local_ep 1 --local_bs 50 --results_save cifar10_fedavg_averagecase_run1
+python3 main_fed.py --seed $SEED --dataset cifar10 --model cnn --num_classes 10 --log_level info --epochs 1000 --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 0.5 --local_ep 1 --local_bs 50 --results_save cifar10_fedavg_averagecase_run1
 
 # "Worst" case
-python3 main_fed.py --seed $SEED --dataset cifar10 --model cnn --num_classes 10 --log_level info --epochs 2000 --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 1 --local_ep 1 --local_bs 50 --results_save cifar10_fedavg_worstcase_run1
+python3 main_fed.py --seed $SEED --dataset cifar10 --model cnn --num_classes 10 --log_level info --epochs 1000 --lr 0.1 --num_users $NUM_USERS --shard_per_user $SHARDS --frac 1 --local_ep 1 --local_bs 50 --results_save cifar10_fedavg_worstcase_run1
 
 # done
